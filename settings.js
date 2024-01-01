f0f117a0411805be3ed248ed97eb77761a12042d
Linkiddrak <114172627+Linkiddrak@users.noreply.github.com>
2024-01-01 19:13:50 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,27 +22,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.ownernomer = "22870035900" //ur owner number2
+global.ownername = "❄️ Raizen Darki Linkid" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.socialm = "GitHub: Linkiddrak" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = "Cheems Bot MD V9"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
+global.botname = "Darki Bot MD V9"
+global.ownernumber = '22870035900'
+global.ownername = '❄️Raizen'
+global.ownerNumber = ["22870035900@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
 global.websitex = "https://youtube.com/@DGXeon"
-global.wagc = "https://chat.whatsapp.com/Kjm8rnDFcpb04gQNSTbW2d"
-global.themeemoji = '🪀'
+global.wagc = "false"
+global.themeemoji = '❄️'
 global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
+global.botscript = 'https://github.com/Linkiddrak/Darkiv9' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
-global.prefa = ['','!','.','#','&']
+global.author = "DarkiLinkid"
+global.creator = "22870035900@s.whatsapp.net"
+global.prefa = ['!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
 
